10645a889a1275586d6f64d1609925427db1facf
Nimra Irfan <nimrairfan43320@gmail.com>
2022-02-21 23:58:09 +0500

    weatherapp

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import './App.css';
 const App = () => {
 
   const [weatherApi , setWeatherApi] = useState({})
-  const [cityName , setCityName] = useState("karachi")
-  const [cityInput , setCityInput] = useState("karachi")
+  const [cityName , setCityName] = useState("lahore")
+  const [searchCityState , setsearchCityState] = useState("lahore")
   useEffect (()=>{
 
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=3d3f17e2394d08edbd30f3817f605a66&unitsmetric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${searchCityState}&appid=3d3f17e2394d08edbd30f3817f605a66&units=metric`
     )
     .then((res) => res.json())
     .then((results)=>{
@@ -20,10 +20,11 @@ const App = () => {
       console.log(error)
     })
   
-} ,[cityName])
+} ,[searchCityState])
 
-const citytype = (inc) => {
-  setCityInput(cityName)
+const searchCity = (inc) => {
+  setsearchCityState(cityName)
+  console.log(cityName)
 }
 
   return (
@@ -32,7 +33,7 @@ const citytype = (inc) => {
         <div className='innerdiv'>
         <input className='cityinput' type="text" value={cityName} placeholder="type Your City" 
           onChange={(inc)=>setCityName(inc.target.value)}/>
-          <button onClick={citytype} className='inputbtn'>Enter</button>
+          <button onClick={searchCity} className='inputbtn'>Enter</button>
           <li>City:- {weatherApi && weatherApi.name}</li>
           <li>Temp:- {weatherApi && weatherApi.main && weatherApi.main.temp}</li>
           <li>Main:- {weatherApi &&weatherApi.weather && weatherApi.weather[0]&& weatherApi.weather[0].main}</li>
@@ -41,6 +42,55 @@ const citytype = (inc) => {
         </div>      
     </div>
   );
+// const [weatherData, setWeatherData] = useState({});
+// const [cityName, setCityName] = useState("karachi");
+// const [searchCityState, setSearchCityState] = useState("karachi");
+// useEffect(() => {
+//   fetch(
+//     `https://api.openweathermap.org/data/2.5/weather?q=${searchCityState}&appid=3d3f17e2394d08edbd30f3817f605a66&units=metric`
+//   )
+//     .then((res) => res.json())
+//     .then((result) => {
+//       setWeatherData(result);
+//     })
+//     .catch((err) => {
+//       console.log("0rrr", err);
+//     });
+// }, [searchCityState]);
+
+// const searchCity = (e) => {
+//   setSearchCityState(cityName);
+// };
+// return (
+//   <div>
+//     <h1>WeatherApp</h1>
+//     <div
+//       className="d-flex justify-content-center my-5"
+//       style={{ flexDirection: "column", alignItems: "center" }}
+//     >
+//       <input
+//         type="text"
+//         value={cityName}
+//         className="form-group form-control w-50"
+//         placeholder="Enter City name"
+//         onChange={(e) => setCityName(e.target.value)}
+//       />
+//       <button onClick={searchCity} className="btn btn-info">
+//         search
+//       </button>
+//     </div>
+//     {/* <button>CALL API</button> */}
+//     <li>CITY: {weatherData && weatherData.name} </li>
+//     <li>TEMP: {weatherData && weatherData.main && weatherData.main.temp} </li>
+//     <li>
+//       Condition:{" "}
+//       {weatherData &&
+//         weatherData.weather &&
+//         weatherData.weather[0] &&
+//         weatherData.weather[0].main}
+//     </li>
+//   </div>
+// );
 }
 
 export default App;
